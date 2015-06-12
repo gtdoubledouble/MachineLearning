cad93192124d47af85236e8a5c81a6a75547e2c3
Gary Tse <garytse89@gmail.com>
2015-06-12 10:26:09 +0800

    Print out accuracy of ML model after feeding in test data

--- ClassifyNB.py
+++ ClassifyNB.py
@@ -11,3 +11,26 @@ def classify(features_train, labels_train):
     
 	clf = GaussianNB()
 	return clf.fit(features_train, labels_train)
+	
+
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example, 
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    accuracy = clf.score(features_train, labels_train)
+    return accuracy
--- naivebayes.py
+++ naivebayes.py
@@ -10,6 +10,7 @@
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture, output_image
 from ClassifyNB import classify
+from ClassifyNB import NBAccuracy
 
 import numpy as np
 import pylab as pl
@@ -30,3 +31,7 @@ clf = classify(features_train, labels_train)
 ### draw the decision boundary with the text points overlaid
 prettyPicture(clf, features_test, labels_test)
 output_image("test.png", "png", open("test.png", "rb").read())
+
+### get accuracy
+accuracy = NBAccuracy(features_train, labels_train, features_test, labels_test)
+print("Accuracy=", accuracy)
